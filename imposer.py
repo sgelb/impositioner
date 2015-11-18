cd954f01f402ca2b3140ca5944beb05d9843cb36
sgelb <stephan.halbritter@haw-hamburg.de>
2015-11-18 13:08:47 +0100

    Add cli options

--- imposer.py
+++ imposer.py
@@ -2,6 +2,7 @@
 
 # First modified: 2015-11-15, 19:10
 
+import argparse
 import os
 import sys
 import math
@@ -13,21 +14,24 @@ def getPageSize(page):
 
 
 def calculateSignatureLength(pageCount):
+    # return pageCount as signatureLength if pageCount too low
     if pageCount <= 36:
+        # make sure that pageCount is even
         if pageCount % 2:
             return pageCount + 1
         else:
             return pageCount
 
-    length = pageCount
+    # calculate signaturelength with as less additional blank pages as possible
+    signatureLength = pageCount
     remainder = sys.maxsize
 
     # possible signature lengths: 20, 24, 28, 32, 36
-    for l in range(16, 36+1, 4):
-        if l - pageCount % l <= remainder:
-            remainder = l - pageCount % l
-            length = l
-    return length
+    for length in range(16, 36+1, 4):
+        if length - pageCount % length <= remainder:
+            remainder = length - pageCount % length
+            signatureLength = length
+    return signatureLength
 
 
 def getSignaturePages(inpages, signatureLength):
@@ -59,19 +63,49 @@ if __name__ == '__main__':
     # - input format to output format + scale or NxN + scale
     # - number of pages per signature
     # - illustrated help for folding/cutting
+    # - add cut line?
     # - creep/shingling/push out?
 
-    if len(sys.argv) != 2:
-        print("Wrong number of arguments!")
-        sys.exit()
+    parser = argparse.ArgumentParser(
+            description='''
+            Impose PDF file.
 
-    infile = sys.argv[1]
+            (TODO: rephrase description)
+
+            Default is to imposition without scaling => -n defines output
+            paper size. Can be overwritten by -p. Automatic calculation of
+            signatures if more than 36 pages. Manual setting of signatures with
+            -s.
+            ''')
+    parser.add_argument('PDF', action='store', help='PDF file')
+    parser.add_argument('-n', dest='nup', action='store', default=2, type=int,
+                        help='pages per sheet (default: 2)')
+    parser.add_argument('-p', dest='papersize', action='store',
+                        choices=['A4', 'A5', 'A6'], help='output paper size '
+                        '(default: auto)')
+    parser.add_argument('-s', dest='signatureLength', action='store', type=int,
+                        help='Set signature length (default: auto)')
+    args = parser.parse_args()
+
+    # validate arguments
+    infile = os.path.abspath(args.PDF)
+    if not os.path.exists(infile):
+        print("File does not exist:", infile)
+        sys.exit(1)
+
+    signatureLength = args.signatureLength
+    if signatureLength and signatureLength % 4:
+        print("Signature length must be multiple of 4!")
+        sys.exit(1)
+
+    # read pdf file
     inpages = PdfReader(infile).pages
 
     # calculate some variables
     pageSize = getPageSize(inpages[0])
     pageCount = len(inpages)
-    signatureLength = calculateSignatureLength(pageCount)
+    if not signatureLength:
+        signatureLength = calculateSignatureLength(pageCount)
     signatureCount = math.ceil(pageCount / signatureLength)
     additionalBlankPagesCount = signatureLength * signatureCount - pageCount
     totalPageCount = pageCount + additionalBlankPagesCount
