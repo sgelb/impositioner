852ca8dfe48106097f2f441e00d059c153e59da5
sgelb <stephan.halbritter@haw-hamburg.de>
2015-11-21 16:28:23 +0100

    Basic imposition

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
-### pdfImposer
+## pdfImposer
 
-Basic imposition of pdfs
+Basic imposition of PDF files
+
+### Features
+
+- simple to print and sort
+- automatic calculation of signature length (can be overwritten)
+- n-up imposition, e.g A6 booklet on A4 paper
+
+### Limitations
+
+- input orientation must be portrait
+- pages per sheet must be a power of 2, e.g. 2, 4, 8 or 16.
+
+### Todo
+
+- [x] basic imposing
+- [x] signatures
+- [ ] user defined output formats
+  - [x] standard papersizes: A0 - A8, letter, ...
+  - [ ] scaling
+  - [ ] resizing
+- [ ] landscape input
+- [ ] fold on short edge
+- [ ] user defined pages per sheet/layout
+- [ ] illustrated help for folding/cutting
+- [ ] add cover from extra pdf file
+- [ ] better name
+
+Thinking about:
+- [ ] support rtl/fold on right side
+- [ ] double/parallel print
+- [ ] add blank pages for easy separation of signatures after printing
+- [ ] add cut line
+- [ ] creep/shingling/push out
--- imposer.py
+++ imposer.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 
-# First modified: 2015-11-15, 19:10
-
 import argparse
 import os
 import sys
@@ -9,18 +7,37 @@ import math
 from pdfrw import PdfReader, PdfWriter, PageMerge
 
 
+paperformats = {
+    'a0': (2384, 3371),
+    'a1': (1685, 2384),
+    'a2': (1190, 1684),
+    'a3': (842, 1190),
+    'a4': (595, 842),
+    'a5': (420, 595),
+    'a6': (298, 420),
+    'a7': (210, 298),
+    'a8': (148, 210),
+    'b4': (729, 1032),
+    'b5': (516, 729),
+    'letter': (612, 792),
+    'legal': (612, 1008),
+    'ledger': (1224, 792),
+    'tabloid': (792, 1224),
+    'executive': (540, 720)
+    }
+
+
 def getPageSize(page):
-    return PageMerge().add(page)[0]['/BBox'][2:]
+    return PageMerge().add(page)[0]['/BBox']
 
 
 def calculateSignatureLength(pageCount):
     # return pageCount as signatureLength if pageCount too low
     if pageCount <= 36:
-        # make sure that pageCount is even
-        if pageCount % 2:
-            return pageCount + 1
-        else:
-            return pageCount
+        # make sure that pageCount is a multiple of 4
+        if pageCount % 4:
+            pageCount += 4 - pageCount % 4
+        return pageCount
 
     # calculate signaturelength with as less additional blank pages as possible
     signatureLength = pageCount
@@ -41,58 +58,87 @@ def getSignaturePages(inpages, signatureLength):
     return signatures
 
 
-def merge(*pages):
-    result = PageMerge() + (x for x in pages if x is not None)
+def split(pages, pagesPerSheet):
+    '''split pages in pagesPerSheet parts of equal lenght'''
+    length = len(pages)
+    npages = []
+    for i in range(pagesPerSheet):
+        start = i * length // pagesPerSheet
+        end = (i + 1) * length // pagesPerSheet
+        npages.append(pages[start:end])
+    return npages
+
+
+def impose(pages, pagesPerSheet):
+    if pagesPerSheet == 1:
+        return pages
+
+    npages = []
+    half = len(pages) // 2
+    rotation = 90 if math.log2(pagesPerSheet) % 2 else 270
+    for i in range(0, half, 2):
+        # frontside
+        npages.append(merge((pages[half+i], pages[i]), rotation))
+        # backside
+        npages.append(merge((pages[i+1], pages[half+i+1]),
+                            (rotation+180) % 360))
+
+    return impose(npages, pagesPerSheet // 2)
+
+
+def merge(pages, rotation):
+    # merge pages, using two rows
+    result = PageMerge() + (page for page in pages)
     result[-1].x += result[0].w
-    result.rotate = 90
-    return result.render()
-
-
-def impose(pages):
-    opages = []
-    while len(pages) > 2:
-        opages.append(merge(pages.pop(), pages.pop(0)))
-        opages.append(merge(pages.pop(0), pages.pop()))
-    opages += pages
-    return opages
+    result = result.render()
+    result.Rotate = rotation
+    return result
 
 
 if __name__ == '__main__':
-    # TODO:
-    # options:
-    # - input format to output format + scale or NxN + scale
-    # - number of pages per signature
-    # - illustrated help for folding/cutting
-    # - add cut line?
-    # - creep/shingling/push out?
-
     parser = argparse.ArgumentParser(
             description='''
             Impose PDF file.
-
-            (TODO: rephrase description)
-
-            Default is to imposition without scaling => -n defines output
-            paper size. Can be overwritten by -p. Automatic calculation of
-            signatures if more than 36 pages. Manual setting of signatures with
-            -s.
             ''')
+
     parser.add_argument('PDF', action='store', help='PDF file')
-    parser.add_argument('-n', dest='nup', action='store', default=2, type=int,
-                        help='pages per sheet (default: 2)')
+    parser.add_argument('-n', dest='nup', action='store', type=int,
+                        default="2", help='pages per sheet (default: 2)')
     parser.add_argument('-p', dest='papersize', action='store',
-                        choices=['A4', 'A5', 'A6'], help='output paper size '
-                        '(default: auto)')
-    parser.add_argument('-s', dest='signatureLength', action='store', type=int,
+                        help='output paper size (default: auto)')
+    parser.add_argument('-s', dest='stretch', action='store_true',
+                        help='stretch pages when resizing. may change aspect '
+                        ' ratio (default: false)')
+    parser.add_argument('-l', dest='signatureLength', action='store', type=int,
                         help='Set signature length (default: auto)')
     args = parser.parse_args()
 
-    # validate arguments
+    # validate infile argument
     infile = os.path.abspath(args.PDF)
     if not os.path.exists(infile):
-        print("File does not exist:", infile)
+        print("File does not exist: {}".format(infile))
         sys.exit(1)
 
+    # validate papersize
+    papersize = args.papersize
+    if papersize and papersize.lower() not in paperformats:
+        print("Unknown papersize: {}. Valid sizes: {}".format(
+            papersize, ', '.join(sorted(paperformats.keys()))))
+        sys.exit(1)
+
+    # validate nup
+    pagesPerSheet = args.nup
+    if pagesPerSheet:
+        if pagesPerSheet < 2:
+            print("Pages per sheet must be a greater than 1, is {}".format(
+                  pagesPerSheet))
+            sys.exit(1)
+        if not math.log2(pagesPerSheet).is_integer():
+            print("Pages per sheet must be a power of 2, is {}".format(
+                  pagesPerSheet))
+            sys.exit(1)
+
+    # validate signatureLength argument
     signatureLength = args.signatureLength
     if signatureLength and signatureLength % 4:
         print("Signature length must be multiple of 4!")
@@ -102,34 +148,48 @@ if __name__ == '__main__':
     inpages = PdfReader(infile).pages
 
     # calculate some variables
-    pageSize = getPageSize(inpages[0])
     pageCount = len(inpages)
     if not signatureLength:
         signatureLength = calculateSignatureLength(pageCount)
     signatureCount = math.ceil(pageCount / signatureLength)
-    additionalBlankPagesCount = signatureLength * signatureCount - pageCount
-    totalPageCount = pageCount + additionalBlankPagesCount
+    # pageSize = getPageSize(inpages[0])
+    # totalPageCount = pageCount + additionalBlankPagesCount
 
     # add blank pages
-    for i in range(additionalBlankPagesCount):
-        inpages.append(None)
+    blankPagesCount = signatureLength * signatureCount - pageCount
+    if blankPagesCount:
+        blank = PageMerge()
+        blank.mbox = inpages[0].MediaBox
+        inpages.extend([blank.render()] * blankPagesCount)
 
+    # impose
     outpages = []
     # split pages in signatures
     for signature in getSignaturePages(inpages, signatureLength):
-        # simple 2x1 imposition
-        outpages.extend(impose(signature))
+        # reverse second half of signature to simplify imposition
+        signature[len(signature)//2:] = list(
+                reversed(signature[len(signature)//2:]))
+
+        # impose each signature
+        outpages.extend(impose(signature, pagesPerSheet))
+
+    # resize
+    if papersize:
+        print("Resize")
+        if args.stretch:
+            # TODO: implement resizing stretching pages
+            print("dont keep ratio")
+        else:
+            # TODO: implement resizing keeping aspect pages
+            print("keep ratio")
+        # o = PageMerge().add(outpages[0])
+        # o[0].scale(0.5)
+        # outpages[0] = o.render()
+        # outpages = resize(outpages, args.papersize)
 
     # save imposed pdf
     outfn = 'booklet.' + os.path.basename(infile)
     print("Save imposed pdf to", outfn)
     PdfWriter().addpages(outpages).write(outfn)
-
-    # print("Page size (pts):", pageSize)
-    # print("Page count:", pageCount)
-    # print("Signature length:", signatureLength)
-    # print("Needed blanks:", additionalBlankPagesCount)
-    # print("Total pages:", pageCount + additionalBlankPagesCount)
-
 
 # vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
